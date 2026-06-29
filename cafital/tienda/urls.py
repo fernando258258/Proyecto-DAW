0e124c6fe12eebfe45065e1dfa0fd5157635e571
Fernando Ricaldes <fernandorashed258@gmail.com>
2026-06-29 19:48:44 -0400

    Sistema de login, registro y logout funcionando

--- cafital/tienda/urls.py
+++ cafital/tienda/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('productos/', views.productos, name='productos'),
     path('sobre-nosotros/', views.sobre_nosotros, name='sobre_nosotros'),
     path('contacto/', views.contacto, name='contacto'),
+    path('registro/', views.registro, name='registro'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- cafital/tienda/views.py
+++ cafital/tienda/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from .models import Producto
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import redirect
+
 
 def index(request):
     return render(request, 'tienda/index.html')
@@ -13,3 +19,38 @@ def sobre_nosotros(request):
 
 def contacto(request):
     return render(request, 'tienda/contacto.html')
+
+def registro(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Ese usuario ya existe.')
+            return redirect('registro')
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        login(request, user)
+        return redirect('index')
+
+    return render(request, 'tienda/registro.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos.')
+            return redirect('login')
+
+    return render(request, 'tienda/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
