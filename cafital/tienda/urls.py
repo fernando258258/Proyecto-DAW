1b2f9ef2cda601e5c47d91ec944a3eb27ba0af07
Fernando Ricaldes <fernandorashed258@gmail.com>
2026-06-29 19:33:50 -0400

    URLs, vistas y templates base funcionando

---
+++ cafital/tienda/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('productos/', views.productos, name='productos'),
+    path('sobre-nosotros/', views.sobre_nosotros, name='sobre_nosotros'),
+    path('contacto/', views.contacto, name='contacto'),
+]
--- cafital/tienda/views.py
+++ cafital/tienda/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Producto
 
-# Create your views here.
+def index(request):
+    return render(request, 'tienda/index.html')
+
+def productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'tienda/productos.html', {'productos': productos})
+
+def sobre_nosotros(request):
+    return render(request, 'tienda/sobre_nosotros.html')
+
+def contacto(request):
+    return render(request, 'tienda/contacto.html')
